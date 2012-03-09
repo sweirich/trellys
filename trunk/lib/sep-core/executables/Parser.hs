a568be7da2303b215b89156dfed9a07182fe35d3
garrin.kimmell <garrin.kimmell@59285afe-504e-ee2a-6240-25eb0d263adb>
2012-03-09 16:48:35 +0000

    Added parser executable skeleton.

--- trunk/lib/sep-core/executables/Parser.hs
+++ trunk/lib/sep-core/executables/Parser.hs
@@ -1,6 +1,15 @@
 module Main where
 
+
+
 import Language.SepCore.Happyparser
+import System.Environment
 
 
-main = putStrLn "Do something"
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> do
+      cnts <- readFile filename
+      putStrLn $ show $ parser $ lexer cnts
+    _ -> putStrLn "usage: sep-core <filename>"
