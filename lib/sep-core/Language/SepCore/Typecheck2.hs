5a1775be032f30fd8000b9674fd0f2992c93291f
fermat1217@gmail.com <fermat1217@gmail.com@59285afe-504e-ee2a-6240-25eb0d263adb>
2012-06-03 05:05:18 +0000

    sync.

--- lib/sep-core/Language/SepCore/Typecheck2.hs
+++ lib/sep-core/Language/SepCore/Typecheck2.hs
@@ -158,13 +158,113 @@ compLK (PredicateApplication p a) = do
         if aeq argclass (ArgClassPredicate theP) then return (subst prt pr lk) else throwError $ disp ("Expected Predicate:")<+>disp(argclass) <+> disp ("Actual predicate:")<+> disp (ArgClassPredicate theP) 
         
 
+compPred :: Proof -> TCMonad Predicate
+
+compPred (PosProof p prf) = compPred prf `catchError` (\ e -> throwError $ disp p <+> e)
+
+-- | Proof_Var
+compPred (ProofVar p) = do 
+  thePred <- (getClass (ArgNameProof p)) >>= ensureArgClassPred
+  compLK thePred
+  return thePred
+  
+-- | Proof_ForallTerm, ForallPredicate, ForallLK
+compPred (ProofLambda b) = do 
+  ((argname, Embed argclass), p) <- unbind b
+  case argclass of
+    ArgClassTerm t -> do  
+      compType t >>= ensureType
+      thePred <- local (M.insert argname (ArgClassTerm t, NonValue)) (compPred p)
+      return (Forall (bind (argname, Embed (ArgClassTerm t)) thePred))
+    ArgClassPredicate pred -> do 
+      compLK pred >>= compSK
+      thePred <- local (M.insert argname (ArgClassPredicate pred, Value)) (compPred p)
+      return (Forall (bind (argname, Embed (ArgClassPredicate pred)) thePred))
+    ArgClassLogicalKind lk -> do 
+       compSK lk 
+       thePred <- local (M.insert argname (ArgClassLogicalKind lk, Value)) (compPred p)
+       return (Forall (bind (argname, Embed (ArgClassLogicalKind lk)) thePred))
+
+-- | Proof_app
+compPred (ProofApplication p a) = do 
+  (Forall b) <- compPred p >>= ensureForall
+  ((argname, Embed argclass),pr) <- unbind b
+  case argname of
+    ArgNameTerm at -> do 
+      t <- ensureArgTerm a 
+      theType <- compType t
+      if aeq argclass (ArgClassTerm theType) then return (subst at t pr) else throwError $ disp ("Expected type: ") <+>disp(argclass)<+> disp("Actual type:")<+> disp (ArgClassTerm theType) 
+    ArgNamePredicate pt -> do
+      pred <- ensureArgPredicate a 
+      theKind <- compLK pred
+      if aeq argclass (ArgClassLogicalKind theKind) then return (subst pt pred pr)else throwError $ disp( "Expected logical kind:")<+>disp(argclass)<+>disp( "Actual kind:")<+> disp(ArgClassLogicalKind theKind)
+    ArgNameProof prt-> do
+      pro <- ensureArgProof a
+      theP <- compPred pro
+      if aeq argclass (ArgClassPredicate theP) then return (subst prt pro pr) else throwError $ disp("Expected Predicate:")<+>disp(argclass)<+> disp(" Actual predicate:")<+> disp(ArgClassPredicate theP) 
+                    
+compType :: Term -> TCMonad Term
+
+compType (Pos p t) = compType t `catchError` (\ e -> throwError $ disp p <+> e)
+
+-- | TERM_TYPE
+compType (Type i)  =  return (Type i)
+
+-- | TERM_VAR
+compType (TermVar var) = do 
+  theType <- (getClass (ArgNameTerm var)) >>= ensureArgClassTerm
+  compType theType >>= ensureType
+  return theType
+                  
+-- | TERM_PI, TERM_PiPredicate, TERM_PILK
+-- compType (Pi b stage) = do 
+--   ((argname, Embed argclass), prog) <- unbind b
+--                            case argclass of
+--                                 ArgClassTerm t -> do theType <- compType t
+--                                                      case theType of
+--                                                        Left (Type 0) -> local (M.insert argname (ArgClassTerm t, NonValue)) (compType prog)
+--                                                        Left _ -> return (Right $ "The type of the term " ++show(t)++" is ill-typed")
+--                                                        Right s -> return (Right s)
+--                                 ArgClassPredicate pred -> do theKind <- compLK pred
+--                                                              case theKind of
+--                                                                Left k -> do sk <- compSK k
+--                                                                             case sk of
+--                                                                               Left (Logical i) -> local (M.insert argname (ArgClassPredicate pred, NonValue)) (compType prog)
+--                                                                               Right s -> return(Right s) 
+--                                                                Right s -> return(Right s)
+--                                 ArgClassLogicalKind lk -> do sk <- compSK lk
+--                                                              case sk of
+--                                                                Left (Logical i) -> local (M.insert argname (ArgClassLogicalKind lk, NonValue)) (compType prog)
+--                                                                Right s -> return(Right s) 
+
+-- -- | Term_LamMinus, LamPlus, LamIMPI, notice here for LamMinus, I implement x \notin FV(t) but not x \notin FV(|t|). 
+-- compType (TermLambda b stage) = do ((argname, Embed argclass), prog) <- unbind b
+--                                    case argclass of
+--                                      ArgClassTerm t -> case stage of
+--                                                     Plus -> do
+--                                                       theType <- local (M.insert argname (ArgClassTerm t, Value)) (compType prog)
+--                                                       case theType of
+--                                                         Left t' -> return (Left(Pi (bind (argname, Embed (ArgClassTerm t)) t') Plus))
+--                                                         Right s -> return (Right s)
+--                                                     Minus -> case argname of
+--                                                                ArgNameTerm tname -> if elem tname (fv prog) then return (Right "undefined")
+--                                                                                 else do theType <- local (M.insert argname (ArgClassTerm t, NonValue)) (compType prog)
+--                                                                                         case theType of
+--                                                                                           Left t' -> return (Left(Pi (bind (argname, Embed (ArgClassTerm t)) t') Minus))
+--                                                                                           Right s -> return (Right s)
+--                                                                _ -> return (Right "undefined")
+--                                      ArgClassPredicate pred -> do 
+--                                               theType <- local (M.insert argname (ArgClassPredicate pred, NonValue)) (compType prog)
+--                                               case theType of
+--                                                 Left ty -> return (Left(Pi (bind (argname, Embed (ArgClassPredicate pred)) ty) Minus))
+--                                                 Right s -> return(Right s)
+--                                      ArgClassLogicalKind lk -> do
+--                                               theType <- local (M.insert argname (ArgClassLogicalKind lk, NonValue)) (compType prog)
+--                                               case theType of
+--                                                 Left ty -> return (Left(Pi (bind (argname, Embed (ArgClassLogicalKind lk)) ty) Minus))
+--                                                 Right s -> return(Right s) 
 
 
-
-
-compPred = undefined
---compLK = undefined
-compType = undefined
 
 -- unWrap the outermost Pos in term.
 unWrapTermPos (Pos _ t) = unWrapTermPos t
@@ -191,8 +291,19 @@ ensureQForall t = case unWrapLKPos t of
                     (QuasiForall b) -> return (QuasiForall b)
                     _ -> throwError $  disp ("Unexpected:")<+> disp t 
 
+ensureForall t = case unWrapPredicatePos t of 
+                    (Forall b) -> return (Forall b)
+                    _ -> throwError $  disp ("Unexpected:")<+> disp t 
+
 ensureArgClassLK (ArgClassLogicalKind lk) = return lk
 ensureArgClassLK t = throwError $  vcat [disp ("Expected:") <+> disp "any LogicalKind", disp ("Actually get:")<+> disp t ]
+
+ensureArgClassPred (ArgClassPredicate pred) = return pred
+ensureArgClassPred t = throwError $  vcat [disp ("Expected:") <+> disp "any Predicate", disp ("Actually get:")<+> disp t ]
+
+ensureArgClassTerm (ArgClassTerm t) = return t
+ensureArgClassTerm t = throwError $  vcat [disp ("Expected:") <+> disp "any Term", disp ("Actually get:")<+> disp t ]
+
 
 ensureArgTerm (ArgTerm t) = return t                                              
 ensureArgTerm t = throwError $  disp ("Unexpected:")<+> disp t 
