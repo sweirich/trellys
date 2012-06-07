db5fb9251c0c5a76efee1a72190e5524f524ea4c
fermat1217@gmail.com <fermat1217@gmail.com@59285afe-504e-ee2a-6240-25eb0d263adb>
2012-06-07 18:36:49 +0000

    sync.

--- trunk/lib/sep-core/Language/SepCore/Rewriting.hs
+++ trunk/lib/sep-core/Language/SepCore/Rewriting.hs
@@ -1,15 +1,31 @@
 module Language.SepCore.Rewriting where
-
+import Language.SepCore.Erasure
 import Language.SepCore.Syntax
 import Language.SepCore.PrettyPrint
 
 import Generics.RepLib hiding (Con(..))
 import Unbound.LocallyNameless hiding (Con(..),Equal,Refl)
-import "mtl" Control.Monad.Trans
+import Control.Monad.Trans
 import Control.Applicative
 import Control.Monad
-import "mtl" Control.Monad.Error
+import Control.Monad.Error
 import Text.PrettyPrint(render, text,(<+>),($$))
 
+
+isNormalForm :: ETerm -> m Bool
+isNormalForm (ETermVar x) = return True
+isNormalForm (EType i) = return True
+isNormalForm (EPi binding s) = do 
+  ((x,Embed t'), t) <- unbind binding
+  isNormalForm t'
+
+isNormalForm (ELambda b) = return True
+isNormalForm (ERec b) = return True
+isNormalForm (EAbort) = return True
+isNormalForm (EApp (ETermVar x) t2) = isNormalForm t2
+isNormalForm _ = return False
+
+
+join t1 t2 = 
 
 
