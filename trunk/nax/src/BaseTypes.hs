ff1edef94e01817ff89481725f63abd9479b2ed3
nathan.collins <nathan.collins@59285afe-504e-ee2a-6240-25eb0d263adb>
2012-06-08 02:49:11 +0000

    Remove unused code.

--- trunk/nax/src/BaseTypes.hs
+++ trunk/nax/src/BaseTypes.hs
@@ -3,16 +3,12 @@ module BaseTypes where
 import qualified Text.PrettyPrint.HughesPJ as PP
 import Text.PrettyPrint.HughesPJ(Doc,text,int,(<>),(<+>),($$),($+$),render)
 
-data Base = Int | Char | Double | Integer | Unit
-  deriving Eq
-
 data Literal 
    = LInt Int                  -- 5 , -5
    | LDouble Double            -- 2.3 , 0.5 , -3.4
    | LChar Char                -- 'x'
    | LInteger Integer
    | LUnit
-
 
 instance Eq Literal where
   (LInt n) == (LInt m) = n==m
@@ -21,13 +17,6 @@ instance Eq Literal where
   (LChar n) == (LChar m) = n==m
   LUnit == LUnit = True
   _ == _ = False
-
-litToBase (LInt n) = Int
-litToBase (LInteger n) = Integer
-litToBase (LDouble n) = Double
-litToBase (LChar c) = Char
-litToBase LUnit = Unit  
-
 
 typeNameLit (LInt n) = "Int"
 typeNameLit (LInteger n) = "Integer"
@@ -41,14 +30,3 @@ ppLit (LInteger n) = text(show n)
 ppLit (LDouble d) = text (show d)
 ppLit (LChar c) = text(show c)
 ppLit (LUnit) = text "()"
-  
-  
--- Base 
-instance Show Base where
-  show Int = "Int"
-  show Char = "Char"
-  show Double = "Double"
-  show Integer = "Integer"
-  show Unit = "()"
-
-
