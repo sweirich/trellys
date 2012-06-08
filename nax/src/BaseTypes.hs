83e44e184c286a48443b69d4c2f5a4207be45478
nathan.collins <nathan.collins@59285afe-504e-ee2a-6240-25eb0d263adb>
2012-06-08 02:49:21 +0000

    Derive 'Eq'.

--- nax/src/BaseTypes.hs
+++ nax/src/BaseTypes.hs
@@ -3,19 +3,12 @@ module BaseTypes where
 import Text.PrettyPrint.HughesPJ (Doc,text)
 
 data Literal 
-   = LInt Int                  -- 5 , -5
-   | LDouble Double            -- 2.3 , 0.5 , -3.4
-   | LChar Char                -- 'x'
+   = LInt Int
+   | LDouble Double
+   | LChar Char
    | LInteger Integer
    | LUnit
-
-instance Eq Literal where
-  (LInt n) == (LInt m) = n==m
-  (LInteger n) == (LInteger m) = n==m
-  (LDouble n) == (LDouble m) = n==m
-  (LChar n) == (LChar m) = n==m
-  LUnit == LUnit = True
-  _ == _ = False
+   deriving Eq
 
 ppLit :: Literal -> Doc
 ppLit (LInt n) = text(show n)
