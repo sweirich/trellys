883642357a357afa7116b3a3e2e705ec606ed32a
fermat1217@gmail.com <fermat1217@gmail.com@59285afe-504e-ee2a-6240-25eb0d263adb>
2012-06-11 17:41:21 +0000

    sync.

--- lib/sep-core/executables/sepcore.hs
+++ lib/sep-core/executables/sepcore.hs
@@ -25,13 +25,14 @@ main = do
       case runAlex cnts parser of
              Left e -> error e
              Right a -> do putStrLn $ "Parsing success! \n" 
-                           unknow <- runErrorT (runFreshMT (runStateT (typechecker a) Data.Map.empty))
+                           unknow <- runErrorT (runFreshMT (runStateT (typechecker a) (Data.Map.empty,Data.Map.empty)))
                            case unknow of
                              Left e -> do
                                putStrLn $ show (disp e)
                              Right (s,env) -> do
                                putStrLn $ show (disp s) ++ "\n"
-                               putStrLn $ show (disp env)
+                               putStrLn $ show (disp (fst env)) ++ "\n"
+                               putStrLn $ show (disp (snd env))
 
 
                            
